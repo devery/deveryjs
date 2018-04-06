a59f605e062f04f43de944a1e776e087719b9d49
victor <victor@victor.local>
2018-04-06 18:56:55 +0400

    adds tests to admined functions

--- devery/devery.js
+++ devery/devery.js
@@ -1,4 +1,6 @@
 import EveToken from './EveToken'
 import DeveryRegistry from './DeveryRegistry'
+import DeveryAdmined from './DeveryAdmined'
 window.DeveryRegistry = DeveryRegistry
 window.EveToken = EveToken
+window.DeveryAdmined = DeveryAdmined
