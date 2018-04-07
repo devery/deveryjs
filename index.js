7fb7e311f7ad7ef50d16bb535835d54cf78f8c88
Victor Eloy <victorrseloy@hotmail.com>
2018-04-08 01:16:57 +0400

    pushing package

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import DeveryRegistry from './devery/DeveryRegistry';
 import DeveryAdmined from './devery/DeveryAdmined';
 import DeveryOwned from './devery/DeveryOwned';
 
-module.exports ={
-    EveToken,
- DeveryRegistry,
- DeveryAdmined,
- DeveryOwned
-}
+
+export const EveToken = EveToken
+export const DeveryRegistry = DeveryRegistry
+export const DeveryAdmined = DeveryAdmined
+export const DeveryOwned = DeveryOwned
