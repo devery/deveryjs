76f28811c988332b0c74732a97a948022e110769
Victor Eloy <victorrseloy@hotmail.com>
2018-04-08 00:47:47 +0400

    packing project

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-import EveToken from './devery/EveToken'
-import DeveryRegistry from './devery/DeveryRegistry'
-import DeveryAdmined from './devery/DeveryAdmined'
-window.DeveryRegistry = DeveryRegistry
-window.EveToken = EveToken
-window.DeveryAdmined = DeveryAdmined
+import EveToken from './devery/EveToken';
+import DeveryRegistry from './devery/DeveryRegistry';
+import DeveryAdmined from './devery/DeveryAdmined';
+import DeveryOwned from './devery/DeveryOwned';
+
+module.exports ={
+    EveToken,
+ DeveryRegistry,
+ DeveryAdmined,
+ DeveryOwned
+}
