ead6a7bbf6291fe05faaf40513f96791fc3d5bfe
Victor Eloy <victorrseloy@hotmail.com>
2018-04-09 16:39:05 +0400

    generate test documentation

--- devery/AbstractDeverySmartContract.js
+++ devery/AbstractDeverySmartContract.js
@@ -3,8 +3,13 @@ const deveryRegistryArtifact = require('../build/contracts/DeveryRegistry.json')
 const ethers = require('ethers');
 const network = { name: 'http://127.0.0.1:8545', chainId: 5777 }
 
-
+/** Class representing a point. */
 export default class AbstractDeverySmartContract extends  AbstractSmartContract{
+    /**
+     * Create a point.
+     * @param {number} x - The x value.
+     * @param {number} y - The y value.
+     */
     constructor(options = {signer:web3,provider:undefined,acc:undefined,address:undefined}){
         super(options)
 
