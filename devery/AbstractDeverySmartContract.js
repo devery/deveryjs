e365b0e085f363876ad3688e3695b7560143ad0d
victor <victor@victor.local>
2018-04-09 16:54:56 +0400

    generates documentation for the project

--- devery/AbstractDeverySmartContract.js
+++ devery/AbstractDeverySmartContract.js
@@ -3,13 +3,14 @@ const deveryRegistryArtifact = require('../build/contracts/DeveryRegistry.json')
 const ethers = require('ethers');
 const network = { name: 'http://127.0.0.1:8545', chainId: 5777 }
 
-/** Class representing a point. */
-export default class AbstractDeverySmartContract extends  AbstractSmartContract{
-    /**
-     * Create a point.
-     * @param {number} x - The x value.
-     * @param {number} y - The y value.
-     */
+/** Abstract class that is base for all the devery registry related smart contracts
+ * There is no reason to directly instantiate it
+ *
+ * @extends AbstractSmartContract
+ */
+
+class AbstractDeverySmartContract extends  AbstractSmartContract{
+
     constructor(options = {signer:web3,provider:undefined,acc:undefined,address:undefined}){
         super(options)
 
@@ -23,3 +24,5 @@ export default class AbstractDeverySmartContract extends  AbstractSmartContract{
             this.__signerOrProvider);
     }
 }
+
+export default AbstractDeverySmartContract
