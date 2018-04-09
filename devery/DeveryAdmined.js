dde46f4bd9e15c59a368664c1aaef248ed0fa43b
Victor Eloy <victorrseloy@hotmail.com>
2018-04-09 23:08:53 +0400

    adds documentation to DeveryOwner, DeveryAdmined and other classes. Improves constructor and avoid instantiation of abstract classes

--- devery/DeveryAdmined.js
+++ devery/DeveryAdmined.js
@@ -1,6 +1,7 @@
 import AbstractDeverySmartContract from './AbstractDeverySmartContract'
 
 
+
 /**
  *
  * Main class to deal with contract administration related operations,
@@ -11,21 +12,26 @@ import AbstractDeverySmartContract from './AbstractDeverySmartContract'
  */
 class DeveryAdmined extends AbstractDeverySmartContract {
 
+
     /**
      *
      * Creates a new instansce of DeveryAdmined
      *
-     * @param options
+     *
+     * @param {ClientOptions} options network connection options
+     *
      */
-    constructor(options = {singer:web3,provider:undefined,acc:undefined,address:undefined}) {
+    constructor(options = {web3Instance:web3,acc:undefined,address:undefined}) {
         super(options)
-
     }
 
     /**
-     * hello
-     * @param addr
-     * @returns {Promise.<*>}
+     *
+     * checks if a given account is admin of the contract
+     *
+     * @param addr target account address
+     * @returns {Promise.<bool,Error>} A promise that resolves to a bool indicating if the requested account
+     * is admin of the contract or an Error if the promisse is rejected
      */
     async isAdmin(addr){
         let result = await this.__deveryRegistryContract.isAdmin(addr);
@@ -34,9 +40,10 @@ class DeveryAdmined extends AbstractDeverySmartContract {
 
 
     /**
-     * hello
-     * @param addr
-     * @returns {Promise.<*>}
+     * makes an account admin of the contract by adding it to the admin array
+     *
+     * @param addr target account address
+     * @returns {Promise.<Transaction,Error>} A promise that resolves to a transaction or an Error if the promisse is rejected
      */
     async addAdmin(addr){
         let result = await this.__deveryRegistryContract.addAdmin(addr);
@@ -44,9 +51,10 @@ class DeveryAdmined extends AbstractDeverySmartContract {
     }
 
     /**
-     * hello
-     * @param addr
-     * @returns {Promise.<*>}
+     * removes admin role from an account
+     *
+     * @param addr target account address
+     * @returns {Promise.<Transaction,Error>} A promise that resolves to a transaction or an Error if the promisse is rejected
      */
     async removeAdmin(addr){
         let result = await this.__deveryRegistryContract.removeAdmin(addr);
@@ -55,18 +63,36 @@ class DeveryAdmined extends AbstractDeverySmartContract {
 
 
     /**
-     * hello
-     * @param addr
-     * @returns {Promise.<*>}
+     * This is a callback function that will be invoked in response to adminEvents
+     *
+     * @callback adminEventCallback
+     * @param {string} address the address that was added or removed from the admin list
+     * (this depends on the type of event that you are listening)
+     */
+
+    /**
+     *
+     * Listener to AdminAdded events, this event triggers whenever a new address is added as admin.
+     * please note that the AdminAddedEventListeners do not stack, this means that whenever you set one you are
+     * removing the last one. If you want to remove a AdminAddedEventListeners, just call this function passing undefined
+     * as param
+     *
+     * @param {adminEventCallback} callback the callback that will be executed whenever and AdminAdded event is
+     * triggered
      */
     setAdminAddedEventListener(callback){
         this.__deveryRegistryContract.onadminadded = callback
     }
 
     /**
-     * hello
-     * @param addr
-     * @returns {Promise.<*>}
+     *
+     * Listener to AdminRemoved events, this event triggers whenever a new address is removed from the admin list.
+     * please note that the AdminRemovedEventListeners do not stack, this means that whenever you set one you are
+     * removing the last one. If you want to remove a AdminRemovedEventListener, just call this function passing undefined
+     * as param
+     *
+     * @param {adminEventCallback} callback the callback that will be executed whenever and AdminRemoved event is
+     * triggered
      */
     setAdminRemovedEventListener(callback){
         this.__deveryRegistryContract.onadminremoved = callback
--- devery/DeveryOwned.js
+++ devery/DeveryOwned.js
@@ -1,11 +1,25 @@
 import AbstractDeverySmartContract from './AbstractDeverySmartContract'
 
-export default class DeveryOwned extends AbstractDeverySmartContract {
+/**
+ *
+ * Main class to deal with the owned smart contract interface and related operations,
+ * you can use it to check the current contract owner and list to ownership change related
+ * events
+ *
+ * @extends AbstractDeverySmartContract
+ */
+class DeveryOwned extends AbstractDeverySmartContract {
 
-    constructor(options = {signer:web3,provider:undefined,acc:undefined,address:undefined}) {
+    /**
+     *
+     * Creates a new instansce of DeveryOwned
+     *
+     *
+     * @param {ClientOptions} options network connection options
+     */
+    constructor(options = {web3Instance:web3,acc:undefined,address:undefined}) {
         super(...arguments)
     }
-
 
     async acceptOwnership(){
         let result = await this.__deveryRegistryContract.acceptOwnership();
@@ -17,10 +31,33 @@ export default class DeveryOwned extends AbstractDeverySmartContract {
         return result.valueOf();
     }
 
+    /**
+     * This is a callback function that will be invoked in response to adminEvents
+     *
+     * @callback OwnershipEventCallback
+     * @param {string} fromAddress
+     * @param {string} toAddress
+     */
+
+    /**
+     *
+     * Listener to OwnershipTransferred events, this event triggers whenever the smart contract ownership changes
+     * (what is a very unlikely event).
+     * please note that OwnershipTransferredEventListeners do not stack, this means that whenever you set one you are
+     * removing the last one. If you want to remove a OwnershipTransferredEventListeners, just call this function passing undefined
+     * as param
+     *
+     * @param {OwnershipEventCallback} callback the callback that will be executed whenever and OwnershipTransferred event is
+     * triggered
+     */
     setOwnershipTransferredListener(callback){
         this.__deveryRegistryContract.onownershiptransferred = callback
     }
 
+    /**
+     *
+     * @returns {Promise.<*>}
+     */
     async getOwner(){
         let result = await this.__deveryRegistryContract.owner();
         return result.valueOf();
@@ -31,3 +68,5 @@ export default class DeveryOwned extends AbstractDeverySmartContract {
         return result.valueOf();
     }
 }
+
+export default DeveryOwned
