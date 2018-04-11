12b7b5e20e606d6fd041e9d51784fbb06611a178
Victor Eloy <victorrseloy@hotmail.com>
2018-04-11 08:30:16 +0400

    improves documentation

--- devery/AbstractDeverySmartContract.js
+++ devery/AbstractDeverySmartContract.js
@@ -5,17 +5,6 @@ const ethers = require('ethers');
 
 
 
-/**
- * @typedef {Object} ClientOptions
- * This object configures how the client will connect and communicate to the Ethereum network,
- * unless you have good reasons to change the default configurations you don't need to worry with any of these values
- * as the will be automatically resolved
- * contract address in your current network. Under normal circunstances you don't need to pass any of these fields.
- * @property {Object} web3Instance  the current web3 object, like the one injected my metamask
- * @property {string} acc the accounts' addres that will execute the transactions
- * @property {string} address expects the contract address in your current network, unless you are running your own
- * network you don't need to provide it
- */
 
 /** Abstract class that is base for all the devery registry related smart contracts
  * There is no reason to directly instantiate it. Here lies some common logic about how to resolve
--- devery/AbstractSmartContract.js
+++ devery/AbstractSmartContract.js
@@ -1,6 +1,25 @@
 const ethers = require('ethers')
 
 
+/**
+ * @typedef {Object} ClientOptions
+ * This object configures how the client will connect and communicate to the Ethereum network,
+ * unless you have good reasons to change the default configurations you don't need to worry with any of these values
+ * as the will be automatically resolved
+ * contract address in your current network. Under normal circunstances you don't need to pass any of these fields.
+ * @property {Object} web3Instance  the current web3 object, like the one injected my metamask
+ * @property {string} acc the accounts' addres that will execute the transactions
+ * @property {string} address expects the contract address in your current network, unless you are running your own
+ * network you don't need to provide it
+ */
+
+/**
+ *
+ * @typedef {Object} TransactionOptions
+ *
+ */
+
+
 class AbstractSmartContract{
     constructor(options = {web3Instance:web3,acc:undefined,address:undefined}){
 
