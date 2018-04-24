8cd4ee74e299e84cec8049c4ed974303a861e120
victor <victorrseloy@hotmail.com>
2018-04-24 09:30:36 +0400

    update package version

--- test/deveryRegistryMark.js
+++ test/deveryRegistryMark.js
@@ -23,31 +23,31 @@ contract('DeveryRegistry - Mark - basic tests', function (accounts) {
         contractAddress = contract.address
 
         let promisseArray = [];
-        for(let account of data){
-            deveryRegistry = createDeveryRegistry(web3,null,account.appAccount,contractAddress)
-            promisseArray.push(deveryRegistry.addApp(account.appName,account.feeAccount,account.fee,overrideOptions))
+        for (let account of data) {
+            deveryRegistry = createDeveryRegistry(web3, null, account.appAccount, contractAddress)
+            promisseArray.push(deveryRegistry.addApp(account.appName, account.feeAccount, account.fee, overrideOptions))
         }
         await Promise.all(promisseArray)
         promisseArray = []
-        for(let account of data){
-            deveryRegistry = createDeveryRegistry(web3,null,account.appAccount,contractAddress)
-            for(let brand of account.brands){
+        for (let account of data) {
+            deveryRegistry = createDeveryRegistry(web3, null, account.appAccount, contractAddress)
+            for (let brand of account.brands) {
                 totalBrands++;
                 brandsArr.push(brand)
-                promisseArray.push(deveryRegistry.addBrand(brand.brandAccount,brand.brandName,overrideOptions))
+                promisseArray.push(deveryRegistry.addBrand(brand.brandAccount, brand.brandName, overrideOptions))
             }
         }
 
         await Promise.all(promisseArray)
         promisseArray = []
 
-        for(let account of data){
-            for(let brand of account.brands){
-                deveryRegistry = createDeveryRegistry(web3,null,brand.brandAccount,contractAddress)
-                for (let product of brand.products){
+        for (let account of data) {
+            for (let brand of account.brands) {
+                deveryRegistry = createDeveryRegistry(web3, null, brand.brandAccount, contractAddress)
+                for (let product of brand.products) {
                     totalProducts++;
                     productsArr.push(product)
-                    promisseArray.push(deveryRegistry.addProduct(product.productAccount,product.description,product.details,product.year,product.origin, overrideOptions))
+                    promisseArray.push(deveryRegistry.addProduct(product.productAccount, product.description, product.details, product.year, product.origin, overrideOptions))
                 }
             }
         }
@@ -58,51 +58,59 @@ contract('DeveryRegistry - Mark - basic tests', function (accounts) {
 
     })
 
-    it('should be possible to brand accounts add permission marker accounts',async function(){
+    it('should be possible to brand accounts add permission marker accounts', async function () {
         let account = data[0]
         let markerAcc = accounts[3];
-        let deveryClient = createDeveryRegistry(web3,undefined,account.appAccount,contractAddress);
-        await deveryClient.permissionMarker(markerAcc,true);
+        let deveryClient = createDeveryRegistry(web3, undefined, account.appAccount, contractAddress);
+        await deveryClient.permissionMarker(markerAcc, true);
         //shall not trow exceptions
     })
 
-    it('should be possible to brand accounts change permission marker accounts',async function(){
+    it('should be possible to brand accounts change permission marker accounts', async function () {
+        let account = data[0]
+        let markerAcc = accounts[3];
+        let deveryClient = createDeveryRegistry(web3, undefined, account.appAccount, contractAddress);
+        await deveryClient.permissionMarker(markerAcc, false);
+        //shall not trow exceptions
+        //maybe try to mark an item
+    })
+
+    it('should compute the hash of an product address', async function () {
+        let account = data[0]
+        let markerAcc = accounts[3];
+        let deveryClient = createDeveryRegistry(web3, undefined, account.appAccount, contractAddress);
+        let hash = deveryClient.addressHash(account.products[0].getAc)
+    })
+
+    it('should be possible to a permissioned account mark an existing product', async function () {
         assert.fail("actual", "expected", "test not implemented");
     })
 
-    it('should compute the hash of an product address',async function(){
+    it('should not be possible to a permissioned account mark an non existing product', async function () {
         assert.fail("actual", "expected", "test not implemented");
     })
 
-    it('should be possible to a permissioned account mark an existing product',async function(){
+    it('should be possible to a non permissioned account mark an existing product', async function () {
         assert.fail("actual", "expected", "test not implemented");
     })
 
-    it('should not be possible to a permissioned account mark an non existing product',async function(){
+    it('should be possible to a non permissioned account mark an existing product', async function () {
         assert.fail("actual", "expected", "test not implemented");
     })
 
-    it('should be possible to a non permissioned account mark an existing product',async function(){
+    it('should be possible to check a marked item', async function () {
         assert.fail("actual", "expected", "test not implemented");
     })
 
-    it('should be possible to a non permissioned account mark an existing product',async function(){
+    it('should be possible to check a non marked item', async function () {
         assert.fail("actual", "expected", "test not implemented");
     })
 
-    it('should be possible to check a marked item',async function(){
+    it('should receive callback when a permission marker account is changed', async function () {
         assert.fail("actual", "expected", "test not implemented");
     })
 
-    it('should be possible to check a non marked item',async function(){
-        assert.fail("actual", "expected", "test not implemented");
-    })
-
-    it('should receive callback when a permission marker account is changed',async function(){
-        assert.fail("actual", "expected", "test not implemented");
-    })
-
-    it('should receive callback when a product is marked',async function(){
+    it('should receive callback when a product is marked', async function () {
         assert.fail("actual", "expected", "test not implemented");
     })
 
