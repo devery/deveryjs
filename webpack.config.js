9fc045237296a2310f2f272be0f7608e93519c68
victor <victorrseloy@hotmail.com>
2018-06-14 13:48:18 +0400

    changes the webpack export

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ module.exports ={
     devtool: 'eval-source-map',
     output: {
         path: path.resolve(__dirname,'dist'),
-        filename: 'index.js'
+        filename: 'index.js',
+        library: "devery",
+        libraryTarget: "umd"
     },
     entry: [path.resolve(__dirname, './index.js')]
 }
