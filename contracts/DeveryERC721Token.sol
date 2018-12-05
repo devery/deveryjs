6f6c4e5b76c047c8ccce2b00fb3bd7b9df86a755
Victor Eloy <victorrseloy@hotmail.com>
2018-12-05 10:17:27 +0100

    adding claim functionlaity to contract

--- contracts/DeveryERC721Token.sol
+++ contracts/DeveryERC721Token.sol
@@ -1,19 +1,25 @@
 pragma solidity ^0.4.24;
+pragma experimental ABIEncoderV2;
+
 
 import "./ERC721.sol";
 import "./DeveryRegistry.sol";
+import "./Admined";
 
-contract DeverERC721Token is ERC721 {
-     DeveryRegistry deveryRegistry;
+contract DeveryERC721Token is ERC721,Admined {
 
-      //TODO: add admin only
-     function setDeveryRegistryAddress(address _deveryRegistryAddress) {
+    address[] tokenIdToProduct;
+    DeveryRegistry deveryRegistry;
+    
+    function setDeveryRegistryAddress(address _deveryRegistryAddress) external onlyAdmin {
         deveryRegistry = DeveryRegistry(_deveryRegistryAddress);
-     }
+    }
 
-    function claimProduct(address _productAddress) external returns (Product) {
-        Product product = deveryRegistry.products(_productAddress);
-        return product;
-
+    function claimProduct(address _productAddress) external view returns (address,address,string,string,uint,string,bool)  {
+        address productBrandAddress;
+        (,productBrandAddress,,,,,) = deveryRegistry.products(_productAddress);
+        require(productBrandAddress == msg.sender);
+        uint nextId = claimedProducts.push(productBrandAddress) - 1;
+        _mint(msg.sender,nextId);
     }
 }
