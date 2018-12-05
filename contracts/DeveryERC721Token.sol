e50c5a9d44151d5cd7b2b711f6deb8f1a7a56c92
Victor Eloy <victorrseloy@hotmail.com>
2018-12-05 10:43:48 +0100

    adding function to get all products of a given owner

--- contracts/DeveryERC721Token.sol
+++ contracts/DeveryERC721Token.sol
@@ -6,20 +6,38 @@ import "./ERC721.sol";
 import "./DeveryRegistry.sol";
 import "./Admined";
 
+/**
+ * @title DeveryERC721Token
+ * @author victor eloy
+ * @dev links the products with our ERC721Token
+ */
 contract DeveryERC721Token is ERC721,Admined {
 
-    address[] tokenIdToProduct;
+
+    address[] public tokenIdToProduct;
     DeveryRegistry deveryRegistry;
-    
+
     function setDeveryRegistryAddress(address _deveryRegistryAddress) external onlyAdmin {
         deveryRegistry = DeveryRegistry(_deveryRegistryAddress);
     }
 
-    function claimProduct(address _productAddress) external view returns (address,address,string,string,uint,string,bool)  {
+    function claimProduct(address _productAddress) external payable  {
         address productBrandAddress;
         (,productBrandAddress,,,,,) = deveryRegistry.products(_productAddress);
         require(productBrandAddress == msg.sender);
         uint nextId = claimedProducts.push(productBrandAddress) - 1;
         _mint(msg.sender,nextId);
+    }
+
+    function getProductsByOwner(address _owner) external view returns (address[]){
+        uint[] products = new address[](_ownedTokensCount[_owner]);
+        uint counter = 0;
+        for(uint i = 0; i < tokenIdToProduct.length;i++){
+            if(_tokenOwner[i] == _owner){
+                products[counter] = tokenIdToProduct[i];
+                counter++;
+            }
+        }
+        return products;
     }
 }
