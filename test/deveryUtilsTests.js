771f5d928cd1b3a5fadee43c0a15fd1945f623da
Victor Eloy <victorrseloy@hotmail.com>
2019-09-30 20:08:15 +0200

    adds test to make sure that we are not generating wrong addresses

---
+++ test/deveryUtilsTests.js
@@ -0,0 +1,12 @@
+import Utils from './../devery/Utils';
+
+
+contract('Utils tests', (accounts) => {
+  it('shall not generete repeated random addresses', () => {
+    const addressArr = [];
+    for (let i = 0; i < 50; i++) {
+      addressArr.push(Utils.getRandomAddress());
+    }
+    assert(new Set(addressArr).size === addressArr.length, 'The resulting set and the original array does not have the same length, this means that there are duplicated items');
+  });
+});
