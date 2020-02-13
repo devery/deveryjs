91e9756797ec5c97d7e5d3856f9d98bd07824f18
Victor Eloy <victorrseloy@hotmail.com>
2020-02-13 07:20:35 +0100

    Revert "pushes changes for webtrust smart contract"
    This reverts commit 81c849038eed42a5ce233201e7dda315c5bc730d.

--- contracts/DeveryTrust.sol
+++ contracts/DeveryTrust.sol
@@ -1,58 +1,21 @@
 pragma solidity ^0.4.18;
 
-import "./DeveryRegistry.sol";
-
-
-
-contract DeveryTrust is Admined {
+contract DeveryTrust {
     mapping(address => mapping(address => bool)) approval;
-    mapping(address => bool) brandApprovals;
-    mapping(address => bool) authorizedApprovers;
     event Approve(address approver, address brandKey);
     event Revoke(address revoker, address brandKey);
 
-    function addAproverAsAdmin(address approverAddress) public onlyAdmin{
-        authorizedApprovers[approverAddress] = true;
-    }
-
-    function revokeAproverAsAdmin(address approverAddress) public onlyAdmin{
-        authorizedApprovers[approverAddress] = false;
-    }
-
-    function addApprover(address approverAddress) public {
-        require(authorizedApprovers[msg.sender] == true,"You are not authorized to add approvers");
-        authorizedApprovers[approverAddress] = true;
-    }
-
-    function revokeAprover(address approverAddress) public{
-        require(authorizedApprovers[msg.sender] == true, "You are not authorized to revoke approvers");
-        authorizedApprovers[approverAddress] = false;
-    }
-
-    function isApprover(address addr) public constant returns (bool){
-        return authorizedApprovers[addr];
-    }
-
-
     function approve(address brandKey) public {
-        require(authorizedApprovers[msg.sender] == true, "You are not authorized approve brands");
         approval[msg.sender][brandKey] = true;
-        brandApprovals[brandKey] = true;
         Approve(msg.sender, brandKey);
     }
-
+    
     function revoke(address brandKey) public {
-        require(authorizedApprovers[msg.sender] == true, "You are not authorized to revoke brands approvals");
         approval[msg.sender][brandKey] = false;
-        brandApprovals[brandKey] = false;
         Revoke(msg.sender, brandKey);
     }
-
+    
     function check(address approver, address brandKey) public constant returns (bool) {
         return approval[approver][brandKey];
-    }
-
-    function checkBrand(address brandKey) public constant returns (bool) {
-        return brandApprovals[brandKey];
     }
 }
