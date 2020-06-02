e19cd57f44f8799ec67090f57dfe1257c732a3aa
Victor Eloy <victorrseloy@hotmail.com>
2020-06-02 11:01:37 +0200

    adds test for address generation

--- test/deveryUtilsTests.js
+++ test/deveryUtilsTests.js
@@ -9,4 +9,15 @@ contract('Utils tests', (accounts) => {
     }
     assert(new Set(addressArr).size === addressArr.length, 'The resulting set and the original array does not have the same length, this means that there are duplicated items');
   });
+
+  it('shall correctly check if an address is valid', () => {
+    for(let i =0; i< 50; i++){
+      assert(Utils.isAddress(Utils.getRandomAddress()));
+    }
+    assert(!Utils.isAddress(123));
+    assert(!Utils.isAddress('OX23232323'));
+    for(let i =0; i< 50; i++){
+      assert(!Utils.isAddress(Utils.getRandomAddress()+'8'));
+    }
+  })
 });
