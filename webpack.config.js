ccfe4f8fd561002ec458909d6d8f7b4b52f46f03
Victor Eloy <victorrseloy@hotmail.com>
2021-03-04 20:22:15 +0100

    makes lib be isomorphic again

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ const webpack = require('webpack')
 
 var libraryName = 'devery';
 
-module.exports ={
+module.exports = [{
     mode:'development',
     devtool: 'eval-source-map',
     target: 'node',
     output: {
         path: path.resolve(__dirname,'dist'),
-        filename: 'index.js',
+        filename: 'index-node.js',
         library: libraryName,
         libraryTarget: "commonjs-module"
     },
@@ -21,4 +21,22 @@ module.exports ={
             }]
     },
     entry: ["babel-polyfill", path.resolve(__dirname, './index.js')]
-}
+},{
+    mode:'development',
+    devtool: 'eval-source-map',
+    target: 'web',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'index-web.js',
+        library: libraryName,
+        libraryTarget: "commonjs-module"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',                
+            }]
+    },
+    entry: ["babel-polyfill", path.resolve(__dirname, './index.js')]
+}]
